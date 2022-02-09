0e782c8b214fd695aa1ea2086066099fc62d3bfc
Gaurav Kumar <84120770+Gauravkr1286@users.noreply.github.com>
2022-02-09 19:47:35 +0530

    Add files via upload
    Output will be:
    Enter the first numb er: 45
    Enter the second number: 5
    The Sum of two numbers is: 50

---
+++ Addwithloop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+       int num1, num2, i; // variable declaration
+       cout << "Enter the first number: ";
+       cin >> num1;
+       cout << "Enter the second number: ";
+       cin >> num2;
+       for (i = 0; i < num2; i++)
+       {
+              num1++;
+       }
+       cout << "Sum of two numbers is: " << num1;
+       getch();
+}
